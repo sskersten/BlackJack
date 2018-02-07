c45c92a740af411d89fadd099c7f442ebfd16c83
Suzy Kersten <suzykersten72@outlook.com>
2018-02-06 21:27:51 -0600

    added about layout

---
+++ app/src/main/java/com/harrisonwelch/blackjack/aboutActivity.java
@@ -0,0 +1,13 @@
+package com.harrisonwelch.blackjack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class aboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
