8f1a5d6e6757bcd580a0b014b67d0a2dbc0ef0c3
Suzy Kersten <suzykersten72@outlook.com>
2018-02-06 21:58:14 -0600

    Added main activity buttons, created button to normal main in debug menu.

--- app/src/main/java/com/harrisonwelch/blackjack/MainActivity.java
+++ app/src/main/java/com/harrisonwelch/blackjack/MainActivity.java
@@ -1,119 +1,55 @@
 package com.harrisonwelch.blackjack;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
-import java.util.Vector;
+/**
+ * Created by Suzanne on 2/6/2018
+ */
 
-public class MainActivity extends Activity {
-
-    private String [] cardNumbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-    private String [] cardSuits = {"Sp","Di","Ht","Cb"};
-    Random random = new Random();
-
-    int handSum = 0;
-    Vector<String> cardsInHand = new Vector<>();
-
+public class MainActivity extends Activity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.i("TAG","onCreate");
-
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_debug);
+        setContentView(R.layout.activity_main);
 
-        setButtons();
-
-    }
-
-    private void setButtons(){
-        Button b = findViewById(R.id.btn_hit);
-        b.setOnClickListener(new View.OnClickListener() {
+        //set up main buttons
+        findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                hitPlayer();
+            public void onClick(View view) {
+                //start gameActivity
+                Toast.makeText(getApplicationContext(), "Not done yet!", Toast.LENGTH_SHORT).show();
             }
         });
-        b = findViewById(R.id.btn_stand);
-        b.setOnClickListener(new View.OnClickListener() {
+
+
+        findViewById(R.id.howToPlay_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                standPlayer();
+            public void onClick(View view) {
+                String url = "https://en.wikipedia.org/wiki/Blackjack";
+                Uri uri = Uri.parse(url);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
+
+        findViewById(R.id.options_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Not done yet!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.about_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), aboutActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    // gives the player a card
-    private void hitPlayer(){
-        Log.i("TAG","msg");
-        TextView handTv = findViewById(R.id.tv_hand);
-        String handStr = handTv.getText().toString();
-
-        // add new card to the table
-        handStr += generateCard(cardNumbers,cardSuits);
-        handTv.setText(handStr);
-        calcTotal();
-    }
-
-    private void standPlayer(){
-
-    }
-
-    // generates a card based on two arrays of nums and suits
-    private String generateCard(String [] nums, String [] suits){
-
-        String str = "";
-
-        // random card number
-        String randCardNum = "";
-//        Log.i("TAG","random.nextInt() % nums.length + 1 : " + (random.nextInt( nums.length)));
-        randCardNum = nums[ random.nextInt(nums.length) ];
-
-        str += randCardNum;
-
-        // then random card face
-        String randCardSuit = "";
-        randCardSuit = suits[ random.nextInt(suits.length) ];
-
-        str += randCardSuit;
-
-        Log.i("TAG","str : " + str);
-
-        cardsInHand.add(str);
-
-        return str;
-    }
-
-    private void playerBusted(){
-        Toast.makeText(getApplicationContext(), "text", Toast.LENGTH_SHORT);
-    }
-
-    private void calcTotal(){
-        int total = 0;
-        for ( int i = 0; i < cardsInHand.size(); i++){
-            String str = cardsInHand.get(i);
-            str = Character.toString(str.charAt(0));
-            int addNum = 0;
-            switch (str){
-                case "1":
-                case "J":
-                case "Q":
-                case "K":
-                case "A":
-                    addNum = 10;
-                    break;
-                default:
-                    addNum = Integer.parseInt(str);
-            }
-            total += addNum;
-        }
-
-        ((TextView) findViewById(R.id.tv_total)).setText(total+"");
-
-    }
 }
