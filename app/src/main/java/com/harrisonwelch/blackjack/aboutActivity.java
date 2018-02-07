e80004f409df4a52ad584d42e3ea307981aec05e
Suzy Kersten <suzykersten72@outlook.com>
2018-02-06 21:29:27 -0600

    made back button work in about layout

--- app/src/main/java/com/harrisonwelch/blackjack/aboutActivity.java
+++ app/src/main/java/com/harrisonwelch/blackjack/aboutActivity.java
@@ -2,6 +2,7 @@ package com.harrisonwelch.blackjack;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class aboutActivity extends Activity {
 
@@ -9,5 +10,12 @@ public class aboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        findViewById(R.id.about_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
